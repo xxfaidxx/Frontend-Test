784d77fceebdcd0b9335a1ae194f3b1dcbd15d4f
Faid <faidwijaya@gmail.com>
2025-07-17 13:37:24 +0700

    banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
+import Banner from "./components/Banner";
 import Ideas from "./components/Ideas";
 
 function App() {
   return (
     <Router>
       <Header />
+      <Banner />
       <Ideas />
     </Router>
   );
